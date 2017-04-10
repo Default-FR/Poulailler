23519f2184e904d42df694504285f891b0554822
Pierre B <pierre.bouillon@openmailbox.org>
2017-04-10 09:35:14 +0200

    Update poulailler.py

--- poulailler.py
+++ poulailler.py
@@ -1,120 +1,118 @@
-import time
-import ephem
+# -*- coding: utf-8 -*-
 
 
-# -------------- VARIABLES -----------------------
-
-#creation d'un observatoire lenoncourt
-lenoncourt = ephem.Observer()
-lenoncourt.pressure = 0
-lenoncourt.horizon = "-6"
-lenoncourt.lat, lenoncourt.lon = '48.665', '6.303'
-
-#pour les cases j[0]m[1]h[2]mn[3] (jour/mois/heure/minute) courantes
-jmhmn = []
-
-#pour les cases j[0]m[1]h[2]mn[3] (jour/mois/heure/minute) de l'alarme
-a_hmn = []
-
-#ouverture matin
-heureMatin = 9
-minuteMatin = 00
-
-
-
-# -------------- FONCTIONS -----------------------
-
-def alarmeSuivante():
-    global a_hmn
-    if int(jmhmn[2])<int(str(lenoncourt.next_setting(ephem.Sun(), use_center=True))[len(str(lenoncourt.next_setting(ephem.Sun(), use_center=True)))-8:len(str(lenoncourt.next_setting(ephem.Sun(), use_center=True)))-6]) and int(jmhmn[2])>=heureMatin:
-        lenoncourt.date=dateCourante()
-        coucher = str(lenoncourt.next_setting(ephem.Sun(), use_center=True))
-        heure = coucher[len(coucher)-8:len(coucher)-6]
-        minute = coucher[len(coucher)-5:len(coucher)-3]
-        a_hmn[:] = [] #on vide la liste
-        a_hmn.append(heure) # heures
-        a_hmn.append(minute) # minutes
-        print "alarme soir set a ",a_hmn[0],":",a_hmn[1]
-    else:
-    	a_hmn[:] = [] #on vide la liste
-        a_hmn.append(heureMatin) # heures
-        a_hmn.append(minuteMatin) # minutes
-        print "alarme matin set a ",a_hmn[0],":",a_hmn[1]
-
-def dateCourante():
-    dateCourante = str(time.localtime()[0])+'/'
-    if time.localtime()[2]<10:
-        dateCourante += '0'
-    dateCourante += str(time.localtime()[2])+'/'
-    if time.localtime()[3]<10:
-        dateCourante += '0'
-    dateCourante +=str(time.localtime()[1])+' 12:00'
-    return dateCourante
-
-
-def sonner(): # Joue le role de l'alarme
-    print("\nAlarme On")
-    print 'Heure actuelle -> mois:', jmhmn[0], ' | jour:', jmhmn[1], ' | heure:', jmhmn[2], ' | minutes:', jmhmn[3]
-    time.sleep(1)
-    print("Alarme Off\n")
-    alarmeSuivante()
-
-def verifAlarme(): # Renvoie vrai si les heures minutes et jours mois correspondent, faux sinon
-    if int(jmhmn[2])==int(a_hmn[0]) and int(jmhmn[3])==int(a_hmn[1]):
-        alarmeSuivante()
-        return True
-    return False
-
-def updateHeure(): # Actualise jmhmn
-    global jmhmn
-    jmhmn[:] = [] #on vide la liste
-    jmhmn.append(time.localtime()[1]) # jours
-    jmhmn.append(time.localtime()[2]) # mois
-    jmhmn.append(time.localtime()[3]) # heures
-    jmhmn.append(time.localtime()[4]) # minutes
-
-
-
-# -------------- MAIN -----------------------
-
-#set de base des alarmes
-updateHeure()
-
-#On initialie avec les heures du soir
-lenoncourt.date=dateCourante()
-a_hmn.append(str(lenoncourt.next_setting(ephem.Sun(), use_center=True))[len(str(lenoncourt.next_setting(ephem.Sun(), use_center=True)))-8:len(str(lenoncourt.next_setting(ephem.Sun(), use_center=True)))-6]) # heures
-a_hmn.append(str(lenoncourt.next_setting(ephem.Sun(), use_center=True))[len(str(lenoncourt.next_setting(ephem.Sun(), use_center=True)))-5:len(str(lenoncourt.next_setting(ephem.Sun(), use_center=True)))-3]) # minutes
-
-#on verifie si l'alarme est bonne
-if int(jmhmn[2])<int(str(lenoncourt.next_setting(ephem.Sun(), use_center=True))[len(str(lenoncourt.next_setting(ephem.Sun(), use_center=True)))-8:len(str(lenoncourt.next_setting(ephem.Sun(), use_center=True)))-6]) and int(jmhmn[2])>=heureMatin:
-    a_hmn[:]=[]
-    lenoncourt.date=dateCourante()
-    coucher = str(lenoncourt.next_setting(ephem.Sun(), use_center=True))
-    heure = coucher[len(coucher)-8:len(coucher)-6]
-    minute = coucher[len(coucher)-5:len(coucher)-3]
-    a_hmn.append(heure) # heures
-    a_hmn.append(minute) # minutes
-
-    print "alarme soir set a ",a_hmn[0],":",a_hmn[1]
-else: #sinon on met celle du matin
-    a_hmn[:]=[]
-    a_hmn.append(heureMatin) # heures
-    a_hmn.append(minuteMatin) # minutes
-    print "alarme matin set a ",a_hmn[0],":",a_hmn[1]
-
-
-#boucle de l'alarme
-while True:
-    updateHeure()
-    #print '\n\nHeure actuelle: \nmois:', jmhmn[0], ' | jour:', jmhmn[1], ' | heure:', jmhmn[2], ' | minutes:', jmhmn[3]
-
-    if verifAlarme():
-        sonner()
-    time.sleep(60)
-
-
-#################################################################
-# Auteur: Pierre Bouillon
+##
+# IMPORTS
 #
-# Date de creation: 01/10/2016
-#################################################################
+
+from ephem      import Observer
+from ephem      import Sun
+from time       import localtime
+from time       import sleep
+
+##
+# CHICKENHOUSE.MAIN
+#
+
+class Alarm:
+    def __init__(self):
+        self.__observer     = self.__builtObs()
+        self.__currentTime  = self.__getCurrentTime()
+        self.__morningAlarm = [9,0]
+        self.__currentAlarm = [0,0]
+        self.__nextAlarm()
+
+    def __str__(self):
+        defObject = "Observer : \n"
+        defObject += "\tlat = "+str(self.__observer.lat)+"\n"
+        defObject += "\tlon = "+str(self.__observer.lon)+"\n\n"
+
+        defObject += "\tMorning alarm = "+str(self.__morningAlarm[0])+ \
+                        ":"+str(self.__morningAlarm[1])+"\n"
+
+        defObject += "\tCurrent alarm = "+str(self.__currentAlarm[0])+ \
+                        ":"+str(self.__currentAlarm[1])+"\n"
+
+        defObject += "\tCurrent time = "+str(self.__currentTime[2])+ \
+                        ":"+str(self.__currentTime[3])+ \
+                        ":"+str(localtime()[5])+"\n" 
+
+        return defObject
+
+    def __builtObs(self):
+        obs          = Observer()
+        obs.pressure = 0
+        obs.horizon  = '-6'
+        obs.lat      = '48.665'
+        obs.lon      = '6.303'
+        return obs
+
+    def __getCurrentTime(self):
+        currentTime = [0,0,0,0]
+        for x in xrange(4):
+            currentTime[x] = localtime()[(x+1)]
+        return currentTime 
+
+    def __getCurrentDate(self):
+        currentDate = str(localtime()[0])+'/'
+        if localtime()[2]<10:
+            currentDate += '0'
+        currentDate += str(localtime()[2])+'/'
+        if localtime()[3]<10:
+            currentDate += '0'
+        currentDate +=str(localtime()[1])+' 12:00'
+        return currentDate
+
+    def __nextAlarm(self):
+        # Set evening alarm
+        obssttg = str(self.__observer.next_setting(
+                        Sun(), use_center=True))
+
+        if  self.__currentTime[2] < \
+            int(obssttg[
+                    len(obssttg)-8:
+                    len(obssttg)-6
+                ]) and self.__currentTime[2]>=self.__morningAlarm[0]:
+        
+            self.__observer.date = self.__getCurrentDate()
+            sunst = str(self.__observer.next_setting(Sun(), 
+                                            use_center=True))
+            self.__currentAlarm[0] = sunst[len(sunst)-8:len(sunst)-6]
+            self.__currentAlarm[1] = sunst[len(sunst)-5:len(sunst)-3]
+
+        # Set morning alarm
+        else:
+            self.__currentAlarm[0] = self.__morningAlarm[0]
+            self.__currentAlarm[1] = self.__morningAlarm[1]
+
+    def __sendNotif(self):
+        pass
+
+    def alarmchck(self):
+        self.__currentTime = self.__getCurrentTime()
+        if self.__currentTime[2]==self.__currentAlarm[0] \
+            and self.__currentTime[3]==self.__currentAlarm[1]:
+            self.__nextAlarm()
+            self.ring()
+
+    def ring(self):
+        sleep(1)
+        self.__sendNotif()
+        self.__nextAlarm()
+
+##
+# FUNCTIONS
+#
+
+def demonize():
+    pass
+
+##
+# MAIN
+#
+if __name__ == "__main__":
+	a = Alarm()
+	while True:
+	    print (a)
+	    a.alarmchck()
+	    sleep(59)
